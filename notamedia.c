0362475827e4b14a7067bb8e8f7b8dd9d9f70ae8
Antonio H. <antonioonizio@gmail.com>
2026-03-06 18:45:04 -0300

    aprimoramento no cod

--- notamedia.c
+++ notamedia.c
@@ -2,16 +2,14 @@
 int main ()
 {
     char nome[15];
-    float n1, n2, soma, media;
+    float n1, n2, media;
     printf("digite seu nome: ");
     scanf("%s", &nome);
     printf("digite sua nota: ");
     scanf("%f", &n1);
     printf("digite sua nota: ");
     scanf("%f", &n2);
-    soma=n1+n2;
-    media=soma/2;
+    media=(n1+n2) /2;
     printf("%s, sua media de notas: %.1f",nome, media);
     return 0;
-
 }
